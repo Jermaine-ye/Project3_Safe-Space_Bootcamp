772a9d7deae4a0131b193823e8da61bcf32b9f18
Jermaine-ye <jer.jmye@gmail.com>
2022-09-19 13:23:02 +0800

    updates

--- safe-space-frontend/src/Components/CalendarDashboard.js
+++ safe-space-frontend/src/Components/CalendarDashboard.js
@@ -1,6 +1,22 @@
 import React from 'react';
 import { Button, Card, Text, Title } from '@mantine/core';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
 
 export default function CalendarDashboard() {
+  const localizer = momentLocalizer(moment);
+
+  const MyCalendar = (props) => (
+    <div>
+      <Calendar
+        localizer={localizer}
+        events={myEventsList}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 500 }}
+      />
+    </div>
+  );
+
   return <div>CalendarDashboard</div>;
 }
