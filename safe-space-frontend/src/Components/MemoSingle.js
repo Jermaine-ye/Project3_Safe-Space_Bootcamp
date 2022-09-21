85303bd94f0ac7f482685a055a60e9eeb9b3205f
Jermaine-ye <jer.jmye@gmail.com>
2022-09-21 10:39:58 +0800

    in progress

--- safe-space-frontend/src/Components/MemoSingle.js
+++ safe-space-frontend/src/Components/MemoSingle.js
@@ -1,5 +1,59 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import { Link, useNavigate, useParams } from 'react-router-dom';
+import {
+  Button,
+  Card,
+  Text,
+  Title,
+  Grid,
+  Container,
+  Form,
+  Input,
+  Textarea,
+} from '@mantine/core';
 
-export default function MemoSingle() {
-  return <div>MemoSingle</div>;
+import { BACKEND_URL } from '../constants.js';
+
+export default function MemoSingle(props) {
+  const params = useParams();
+
+  // useEffect(() => {
+  //   setSightingIndex(id);
+  // }, []);
+
+  // useEffect(() => {
+  //   getSightingsData();
+  // }, [sightingIndex]);
+
+  return (
+    <div>
+      <div>
+        {console.log(sighting, sightingIndex)}
+
+        {Object.keys(sighting).length > 0 ? (
+          <div>
+            <div>
+              <p>Report Number: {sighting.REPORT_NUMBER}</p>
+            </div>
+            <div>
+              <p>
+                Date: {sighting.DATE}/{sighting.MONTH}/{sighting.YEAR}
+              </p>
+            </div>
+            <div>
+              <p>
+                Area: {sighting.STATE}/{sighting.COUNTY}
+              </p>
+            </div>
+            <div>
+              <p>Witness Report: {sighting.OBSERVED}</p>
+            </div>
+          </div>
+        ) : (
+          <p>Data Unavailable</p>
+        )}
+      </div>
+    </div>
+  );
 }
