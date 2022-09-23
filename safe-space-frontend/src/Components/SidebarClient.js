92af42d019c1858ddbfc0df863d752dce6f18389
WorkOutWorkIn <mingshengho96@gmail.com>
2022-09-23 19:08:42 +0800

    sidebar minor edit

--- safe-space-frontend/src/Components/SidebarClient.js
+++ safe-space-frontend/src/Components/SidebarClient.js
@@ -1,6 +1,11 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 
 export default function SidebarClient() {
+  const [name, getName] = useState();
+  const { user } = useAuth0();
+
+  useEffect(() => {});
   //loggout Auth0 will be here
-  return <div>SidebarClient</div>;
+  return <div></div>;
 }
